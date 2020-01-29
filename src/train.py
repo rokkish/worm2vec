9681cc892530613239ebd975f2898c2826f9c0c8
hiro <hiro@ou_desk>
2020-01-29 16:24:47 +0900

    add regulation, add plot embedding

--- src/train.py
+++ src/train.py
@@ -74,41 +74,52 @@ def train(epoch, vae, train_loader, optimizer, writer, device):
         KLD = -0.5 * torch.mean(torch.mean(1 + logvar - mu.pow(2) - logvar.exp(), 1))
         return BCE, KLD * 0.1
 
+    def rotation_invariance_regularization(target, vae, z):
+        RI = 0
+        for theta in range(target.shape[1]):
+            mu, logvar = vae.encode(target[:, theta])
+            mu, logvar = mu.squeeze(), logvar.squeeze()
+            z_theta = vae.reparameterize(mu, logvar)
+            RI += torch.norm(z - z_theta)
+        return RI / target.shape[0] / target.shape[1] * config.lambda_ri
+
     vae.train()
 
     #logger.debug("Load data")
     for batch_idx, (data, target) in enumerate(train_loader):
-        if batch_idx == config.BATCH_SIZE//2 or batch_idx == 0:
-            logger.debug("Train batch: %d/%d " % (batch_idx + 1, len(train_loader)))
-
         data = data.to(device)
+        target = target.to(device)
 
         optimizer.zero_grad()
 
-        #logger.debug("foward")
+        #logger.debug("forward")
         rec, mu, logvar = vae(data)
+        z = vae.reparameterize(mu, logvar)
 
         #logger.debug("get loss")
         loss_re, loss_kl = loss_function(rec, data, mu, logvar)
+        loss_ri = rotation_invariance_regularization(target, vae, z)
 
         #logger.debug("backward")
-        (loss_re + loss_kl).backward()
+        (loss_re + loss_kl + loss_ri).backward()
 
         optimizer.step()
 
-        if batch_idx == config.BATCH_SIZE//2 or batch_idx == 0:
-            logger.debug("Train batch: [{}/{} ({:.0f}%)]\tLoss_re: {:.6f} \tLoss_kl: {:.6f}".format(
+        if batch_idx % (len(train_loader) // 10) == 0:
+            logger.debug("Train batch: [{}/{} ({:.0f}%)]\tLoss_re: {:.5f} \tLoss_kl: {:.5f} \tLoss_ri: {:.5f}".format(
                     batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss_re.item(), loss_kl.item()))
+                    100. * batch_idx / len(train_loader), loss_re.item(), loss_kl.item(), loss_ri.item()))
 
         writer.add_scalar(tag="train_loss/re", scalar_value=loss_re.item(), global_step=batch_idx)
         writer.add_scalar(tag="train_loss/kl", scalar_value=loss_kl.item(), global_step=batch_idx)
+        writer.add_scalar(tag="train_loss/ri", scalar_value=loss_ri.item(), global_step=batch_idx)
 
     return loss_re, loss_kl
 
 def evaluate(epoch, vae, test_loader, writer, device):
     if epoch % 10 != 0:
-        return
+        pass
+    Embedding = True
 
     sample_v = torch.randn(256, config.z_size).view(-1, config.z_size, 1, 1)
 
@@ -116,18 +127,31 @@ def evaluate(epoch, vae, test_loader, writer, device):
         vae.eval()
 
         for batch_idx, (x, target) in enumerate(test_loader):
-            # Reconstruction from testdata
+            # Embedding from testdata
             x = x.to(device)
-            x_rec, _, _ = vae.forward(x)
+            mu, logvar = vae.encode(x)
+            mu, logvar = mu.squeeze(), logvar.squeeze()
+            z = vae.reparameterize(mu, logvar)
 
             x = (x - x.min()) / (x.max() - x.min())
-            x_rec = (x_rec - x_rec.min()) / (x_rec.max() - x_rec.min())
+            z = (z - z.min()) / (z.max() - z.min())
 
-            resultsample = torch.cat([x, x_rec])
-            resultsample = resultsample.cpu()
+            logger.debug(x.shape) # [64, 1, 64, 64]
+            logger.debug(z.shape) # [64, 64]
 
-            save_images_grid(resultsample, nrow=16, scale_each=True,
-                        filename=r'../results/training/{:0=3}_sample_encode.png'.format(epoch))
+            if Embedding:
+                meta = []
+                for i in range(x.shape[0]):
+                    meta.append(str(i))
+                writer.add_embedding(mat=z, metadata=meta, label_img=x)
+
+            else:
+
+                resultsample = torch.cat([x, z])
+                resultsample = resultsample.cpu()
+
+                save_images_grid(resultsample, nrow=16, scale_each=True,
+                            filename=r'../results/training/{:0=3}_sample_encode.png'.format(epoch))
 
             # Reconstruction from random (mu, var)
             sample_v = sample_v.to(device)
@@ -145,14 +169,12 @@ def evaluate(epoch, vae, test_loader, writer, device):
     #    writer.add_graph(vae, sample_v)
 
 def main(args, device):
-    """ load datasets """
     logger.info("Begin train")
     train_loader, test_loader = load_processed_datasets(args.traindir)
 
     # start tensorboard
     writer = SummaryWriter(log_dir="../log/tensorboard/" + args.logdir, comment=args.comment)
 
-    # Def model
     logger.info("define model")
     vae = VAE(zsize=config.z_size, layer_count=config.layer_count, channels=1)
     print(vae)
@@ -160,7 +182,6 @@ def main(args, device):
 
     optimizer = optim.SGD(vae.parameters(), lr=0.01, momentum=0.9)
 
-    # Train model
     for epoch in range(1, args.epoch + 1):
         
         logger.info("Epoch: %d/%d" % (epoch, args.epoch))
