323d3e54ea8016abce70c04da4e1efafec7137f6
hiroki <hiroki@tsujiserver>
2020-08-18 19:19:25 +0900

    add set_placeholders()

--- test/harmonic/worm2vec/run_worm2vec.py
+++ test/harmonic/worm2vec/run_worm2vec.py
@@ -2,6 +2,7 @@
 """
 import os
 import hydra
+import tensorflow as tf
 from omegaconf import DictConfig
 import get_logger
 logger = get_logger.get_logger(name='run')
@@ -14,12 +15,24 @@ def add_folder(folder_name):
     return folder_name
 
 
+def set_placeholders(batch_size, dim):
+    x = tf.placeholder(tf.float32,
+                       [batch_size, dim**2], name='x')
+    learning_rate = tf.placeholder(tf.float32,
+                                   name='learning_rate')
+    train_phase = tf.placeholder(tf.bool, name='train_phase')
+    return {"x": x,
+            "learning_rate": learning_rate,
+            "train_phase": train_phase}
+
+
 @hydra.main(config_path="./conf/config.yaml")
 def main(cfg: DictConfig):
     logger.info("Begin run")
 
     # load_data
     # build model
+    placeholders = set_placeholders(cfg.nn.batch_size, cfg.nn.dim)
     # train
     # test
 
