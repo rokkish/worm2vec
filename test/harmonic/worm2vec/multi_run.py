129e8905522f6a391e2a9f9e7243072f3b35ca3f
hiroki <hiroki@tsujiserver>
2020-09-16 14:48:45 +0900

    add multi_run for train all data

---
+++ test/harmonic/worm2vec/multi_run.py
@@ -0,0 +1,63 @@
+"""run run_worm2vec.py with multi-dataset"""
+import subprocess
+import glob
+import get_logger
+logger = get_logger.get_logger(name="multi_run")
+
+
+def glob_prev_datetime():
+    """
+    Returns:
+        [str]: "2020-09-01/00-00-00"
+    """
+    root = "/root/worm2vec/worm2vec/test/harmonic/worm2vec/outputs"
+    date_dir = sorted(glob.glob(root + "/*"))
+    prev_date_dir = date_dir[-1]
+    time_dir = sorted(glob.glob(prev_date_dir + "/*"))
+    prev_datetime_dir = time_dir[-1]
+
+    logger.debug(prev_datetime_dir)
+
+    return prev_datetime_dir
+
+
+if __name__ == "__main__":
+
+    logger.info("prev dir: {}".format(glob_prev_datetime()))
+
+    # glob data
+    data = glob.glob("/root/worm2vec/data/variety_data_r36_n5_np_*_*/*.npz")
+    data = sorted(data)
+    if len(data) == 0:
+        raise ValueError("data not found")
+
+    epoch = 10
+
+    prev_date = glob_prev_datetime()
+
+    for i, data_i in enumerate(data):
+
+        logger.info("train: {}/{}".format(i+1, len(data)))
+        # skip
+        if i < 12:
+            continue
+        # run
+        if i == 0:
+            subprocess.call(["python", "run_worm2vec.py",
+                "path.worm_data={}".format(data_i),
+                "nn.n_epochs={}".format(epoch),
+                "nn.batch_size=1",
+                "train.restart_train=False",
+            ])
+            prev_date = glob_prev_datetime()
+        # reload params
+        else:
+            subprocess.call(["python", "run_worm2vec.py",
+                "path.worm_data={}".format(data_i),
+                "nn.n_epochs={}".format(epoch),
+                "nn.batch_size=1",
+                "train.restart_train=True",
+                "path.checkpoint_fullpath={}/checkpoints/model.ckpt".format(prev_date)
+            ])
+            prev_date = glob_prev_datetime()
+
