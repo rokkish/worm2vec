8dd53bbd8ba466b6d5b175fbca93390035c8b40b
docker hiroki <hiroki@tsuji_docker>
2020-12-22 17:53:58 +0900

    refactor, add hydra

--- test/sequential/2dshape/preprocess.py
+++ test/sequential/2dshape/preprocess.py
@@ -3,18 +3,25 @@
 import os
 from PIL import Image
 import numpy as np
+import hydra
+from omegaconf import DictConfig
+import get_logger
+logger = get_logger.get_logger(name='preprocess')
 
 
 class Preprocess(object):
-    def __init__(self):
-        self.data_path = "./data/raw/"
-        self.np_path = "./data/raw_np/"
-        self.save_path = "./data/processed/"
+    def __init__(self, cfg):
+        original_cwd = hydra.utils.get_original_cwd()
+        logger.info(original_cwd)
+        self.data_path = original_cwd + cfg.dir.data
+        self.np_path = original_cwd + cfg.dir.np
+        self.save_path = original_cwd + cfg.dir.save
         self.data = {}
         self.fig = ["circle", "square", "triangle"]
         self.min_data_length = 10**4
-        self.resize_width = 32
-        self.resize_height = 32
+        self.resize_width = cfg.params.resize_width
+        self.resize_height = cfg.params.resize_height
+        self.num_mean_img = cfg.params.num_mean_img
         # flag
         self.translate_into_np_is_needed = False
 
@@ -82,17 +89,24 @@ class Preprocess(object):
         self.pair_list_figures = itertools.combinations(self.fig, 2)
 
     def compute_mean_pair(self, parameter_list=None):
+        # num_mean: 中間の画像数
+        # num_all: 前後と中間の画像数
+        num_mean = self.num_mean_img
+        num_all = num_mean + 2
+        w = self.resize_width
+        h = self.resize_height
+
         for (fig_i, fig_j) in self.pair_list_figures:
-            arr_j = np.zeros((self.min_data_length, 11, self.resize_width, self.resize_height))
+            arr_j = np.zeros((self.min_data_length, num_all, w, h))
             os.makedirs("{}{}_{}".format(self.save_path, fig_i, fig_j), exist_ok=True)
 
             for j, path_j in enumerate(self.data[fig_j]):
                 arr_j[j] += np.load(path_j)
 
             for i, path_i in enumerate(self.data[fig_i]):
-                arr = np.zeros((self.min_data_length, 11, self.resize_width, self.resize_height))
+                arr = np.zeros((self.min_data_length, num_all, w, h))
                 np_i = np.load(path_i)
-                for beta in range(0, 11):
+                for beta in range(0, num_all):
                     arr[:, beta] += (1 - beta * 0.1) * np_i
                     arr[:, beta] += beta * 0.1 * arr_j[:, beta]
                 arr /= 2
@@ -100,8 +114,10 @@ class Preprocess(object):
                 print("\r {}_{} {}/{}".format(fig_i, fig_j, i, len(self.data[fig_i])), end="")
 
 
-def main():
-    preprocessor = Preprocess()
+@hydra.main(config_path="conf", config_name="config_preprocess")
+def main(cfg: DictConfig):
+    logger.info(cfg)
+    preprocessor = Preprocess(cfg)
     preprocessor.run()
 
 
