88c5da8a876ec2e7ef120d3358749111a7106cc5
docker hiroki <hiroki@tsuji_docker>
2021-01-14 17:33:44 +0900

    add: Trainer.predict()

--- test/sequential/2dshape/trainer.py
+++ test/sequential/2dshape/trainer.py
@@ -57,6 +57,19 @@ class Trainer(object):
         # morph or minimorph for switch minibatcher
         self.dataset_name = params.dir.data.split("/")[-1]
 
+        ### predict ###
+        self.default_logdir = params.dir.tensorboard
+        self.layers_name = [
+            "flatten/Reshape:0",
+            "concat_outputs/concat:0"
+        ]
+        if self.dataset_name == "morph":
+            self.n_embedding = 27
+        elif self.dataset_name == "minimorph":
+            self.n_embedding = int(params.training.n_samples*params.training.test_rate)//params.training.batchsize*3
+        self.output_dim = 256
+        self.constant_idx = 0
+
     def init_session(self):
         """Initial tensorflow Session
 
@@ -150,9 +163,132 @@ class Trainer(object):
             # save models
             saver_fullmodel.save(sess, self.checkpoint)
 
+            self.predict(sess, data, epoch)
+
             epoch += 1
 
             logger.debug('[{:04d} | {:04.1f}] Train loss: {:04.8f}, Test loss: {:04.8f}'.format(epoch, time.time() - init_t, train_loss, test_loss))
+
+        sess.close()
+
+    def predict(self, sess, data, epoch):
+        from visualize.sprite_images import create_sprite_image, save_sprite_image
+        from tensorboard.plugins import projector
+
+        self.logdir = "{}projector/{:0=2}/".format(self.default_logdir, epoch)
+        os.makedirs(self.logdir, exist_ok=True)
+
+        medium_op = list(
+            map(
+                lambda tname: tf.get_default_graph().get_tensor_by_name(tname),
+                self.layers_name)
+            )
+
+        batcher = self.minibatcher_withlabel(data["test_x"], data["test_label"])
+
+        # def zero vec
+        prev_context_summary_np = np.zeros((self.n_embedding, self.output_dim))
+        next_context_summary_np = np.zeros((self.n_embedding, self.output_dim))
+        prev_target_summary_np = np.zeros((self.n_embedding, self.output_dim))
+        next_target_summary_np = np.zeros((self.n_embedding, self.output_dim))
+        shapetarget_summary_np = np.zeros((self.n_embedding, self.output_dim))
+        prev_context_img = np.zeros((self.n_embedding, self.size, self.size))
+        next_context_img = np.zeros((self.n_embedding, self.size, self.size))
+        target_img = np.zeros((self.n_embedding, self.size, self.size))
+        shapetarget_img = np.zeros((self.n_embedding, self.size, self.size))
+
+        labels = []
+
+        for i, (x_previous, x_now, x_next, x_label) in enumerate(batcher):
+            if i == self.n_embedding:
+                break
+
+            feed_dict = {
+                self.x_previous: x_previous,
+                self.x_now: x_now,
+                self.x_next: x_next,
+                self.learning_rate: self.lr
+            }
+            summary = sess.run([medium_op], feed_dict=feed_dict)
+
+            # select last layer output
+            summary_np = np.array(summary[0][-1])
+            # select first layer output
+            summary_np2 = np.array(summary[0][0])
+
+            # cat several vectors (output of model)
+            prev_context_summary_np[i: (i+1)] = summary_np[self.constant_idx]
+            next_context_summary_np[i: (i+1)] = summary_np[self.constant_idx]
+            prev_target_summary_np[i: (i+1)] = summary_np[2*self.batchsize + self.constant_idx]
+            next_target_summary_np[i: (i+1)] = summary_np[2*self.batchsize + self.constant_idx]
+            shapetarget_summary_np[i: (i+1)] = summary_np2[self.constant_idx]
+
+            # cat several images
+            prev_context_img[i: (i+1)] = x_previous[self.constant_idx]
+            next_context_img[i: (i+1)] = x_next[self.constant_idx]
+            target_img[i: (i+1)] = x_now[self.constant_idx]
+            shapetarget_img[i: (i+1)] = x_now[self.constant_idx]
+
+            labels.append(x_label)
+
+        cat_tensors = np.concatenate([
+            prev_context_summary_np,
+            next_context_summary_np,
+            prev_target_summary_np,
+            next_target_summary_np,
+            shapetarget_summary_np
+            ], axis=0)
+        cat_images = np.concatenate([
+            prev_context_img,
+            next_context_img,
+            target_img,
+            target_img,
+            shapetarget_img
+            ], axis=0)
+        cat_labels = labels + labels + labels + labels + labels
+
+        # tensorize
+        variables = tf.Variable(cat_tensors, trainable=False, name="embedding")
+
+        # make tensor.tsv
+        df = pd.DataFrame(cat_tensors).astype("float64")
+        df.to_csv("{}tensor.csv".format(self.logdir), header=False, index=False, sep="\t")
+
+        # make metadata.tsv (labels)
+        label_dict = {
+            "square_circle": 0,
+            "triangle_circle": 1,
+            "square_triangle": 2
+        }
+        with open("{}metadata.tsv".format(self.logdir), "w") as f:
+            f.write("Index\tLabel\tPrevContext_NextContext_PrevTarget_NextTarget_ShapeTarget\n")
+
+            for index, label in enumerate(cat_labels):
+                id_label = label_dict[label]
+
+                context_or_target_label = index // self.n_embedding
+
+                f.write("%d\t%d\t%d\n" % (index, id_label, context_or_target_label))
+
+        # make sprite image (labels)
+        cat_images = (1 - cat_images) * 255.
+        save_sprite_image(create_sprite_image(cat_images), path="{}sprite.png".format(self.logdir))
+
+        # config of projector
+        config_projector = projector.ProjectorConfig()
+        embedding = config_projector.embeddings.add()
+        embedding.tensor_name = variables.name
+
+        # tsv path
+        embedding.tensor_path = "tensor.csv"
+        embedding.metadata_path = "metadata.tsv"
+
+        # config of sprite image
+        embedding.sprite.image_path = "sprite.png"
+        embedding.sprite.single_image_dim.extend([self.size, self.size])
+
+        summary_writer = tf.summary.FileWriter(self.logdir, sess.graph)
+        projector.visualize_embeddings(summary_writer, config_projector)
 
     def minibatcher(self, inputs):
         """Yield batchsize data.
@@ -187,3 +323,39 @@ class Trainer(object):
                 x_next = np.reshape(x[:, 2], (bs, size, size))
 
                 yield x_previous, x_now, x_next
+
+    def minibatcher_withlabel(self, inputs, labels):
+        """
+
+        Args:
+            inputs (list): list of data path. (*.npy)
+            shuffle (bool, optional): shffule idx. Defaults to False.
+
+        Yields:
+            x_previous, x_now, x_next, label (ndarray):
+        """
+        size = self.size
+        bs = self.batchsize
+
+        for idx in range(0, len(inputs), bs):
+
+            x = inputs[idx: idx + bs]
+
+            label = labels[idx].split("/")[-2]
+
+            if self.dataset_name == "morph":
+
+                for m in range(0, x.shape[1]-2):
+                    x_previous = np.reshape(x[:, 0+m], (bs, size, size))
+                    x_now = np.reshape(x[:, 1+m], (bs, size, size))
+                    x_next = np.reshape(x[:, 2+m], (bs, size, size))
+
+                    yield x_previous, x_now, x_next, label
+
+            elif self.dataset_name == "minimorph":
+
+                x_previous = np.reshape(x[:, 0], (bs, size, size))
+                x_now = np.reshape(x[:, 1], (bs, size, size))
+                x_next = np.reshape(x[:, 2], (bs, size, size))
+
+                yield x_previous, x_now, x_next, label
