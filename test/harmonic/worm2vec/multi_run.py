0019281c1f6fe025a6153ee0cef28ebb5cdefaf1
docker hiroki <hiroki@tsuji_docker>
2021-01-24 11:45:33 +0900

    add: exp_name

--- test/harmonic/worm2vec/multi_run.py
+++ test/harmonic/worm2vec/multi_run.py
@@ -39,6 +39,8 @@ if __name__ == "__main__":
     n_classes = 10
     n_negative = 70
     fixedtestdata = "/root/worm2vec/data/variety_data_strict_r36_n100_np/minitest/000_2000.npz"
+    exp_name = "fulltrain neegative_top70"
+    exp_name_ = "plt " + exp_name
 
     runned_datetime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
     os.makedirs("/root/worm2vec/worm2vec/test/harmonic/worm2vec/logs/test_score/{}".format(runned_datetime))
@@ -49,7 +51,7 @@ if __name__ == "__main__":
         post("train: {}/{}".format(i+1, len(data)))
 
         # skip
-        if i in []:
+        if i in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
             prev_date = glob_prev_datetime()
             continue
 
@@ -64,6 +66,7 @@ if __name__ == "__main__":
                 "nn.n_negative={}".format(n_negative),
                 "path.test_score=/root/worm2vec/worm2vec/test/harmonic/worm2vec/logs/test_score/{}/cossim_{:0=2}.csv".format(runned_datetime, i),
                 "nn.n_classes={}".format(n_classes),
+                "exp_name={}".format(exp_name),
             ])
             prev_date = glob_prev_datetime()
         # reload params
@@ -78,10 +81,12 @@ if __name__ == "__main__":
                 "nn.n_negative={}".format(n_negative),
                 "path.test_score=/root/worm2vec/worm2vec/test/harmonic/worm2vec/logs/test_score/{}/cossim_{:0=2}.csv".format(runned_datetime, i),
                 "nn.n_classes={}".format(n_classes),
+                "exp_name={}".format(exp_name),
             ])
             prev_date = glob_prev_datetime()
 
         # predict
+        """
         subprocess.call(["python", "run_worm2vec.py",
             "path.worm_data={}".format(data_i),
             "path.fixedtestdata={}".format(fixedtestdata),
@@ -91,6 +96,7 @@ if __name__ == "__main__":
             "path.tensorboard=./",
             "train_mode=False",
             "nn.n_classes={}".format(n_classes),
+            "exp_name={}".format(exp_name_),
         ])
         predict_date = glob_prev_datetime()
 
@@ -98,3 +104,4 @@ if __name__ == "__main__":
         load_checkpoints_dir = "{}/checkpoints/".format(prev_date)
         cp_checkpoints_dir = "{}/checkpoints/".format(predict_date)
         copy_tree(load_checkpoints_dir, cp_checkpoints_dir)
+        """
